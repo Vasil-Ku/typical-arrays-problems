e0dd7e98f1eb4e06de6b5d3c4c9645a2645b1cff
Vasil-Ku <vaskuch84@gmail.com>
2020-10-03 15:12:39 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array.length == 0) {
+    return 0;
+  }
+  else {
+    let m = array[0];
+    for (let i = 1; i < array.length; i++) {
+      if (m > array[i]) m = array[i];
+    }
+    return m;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array.length == 0) {
+    return 0;
+  }
+  else {
+    let m = array[0];
+    for (let i = 1; i < array.length; i++) {
+      if (m < array[i]) m = array[i];
+    }
+    return m;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array.length == 0) {
+    return 0;
+  }
+  else {
+    let s = 0;
+    for (let i = 0; i < array.length; i++) {
+      s += array[i];
+    }
+    s = s / array.length
+    return s;
+  }
 }
